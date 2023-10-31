03cf1a37f82c6e9f4ddc4a4d952c538a52f403fa
jhovana <flores.jhovana.644@gmail.com>
2023-10-31 16:02:28 -0400

    nuevos cambios

--- application/views/incltever/cabecera.php
+++ application/views/incltever/cabecera.php
@@ -4,7 +4,7 @@
 
 <head>
     <meta charset="utf-8">
-    <title>Sistema Loreto RE OK</title>
+    <title>Sistema Loreto RE OKkk</title>
     <meta content="width=device-width, initial-scale=1.0" name="viewport">
     <meta content="" name="keywords">
     <meta content="" name="description">
